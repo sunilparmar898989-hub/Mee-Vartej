cef1e3d050101a0d53ce430121e9fdffa0cbc264
sunilparmar898989-hub <sunil.parmar898989@gmail.com>
2026-09-13 17:17:01 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    tanstackStart(),
+    react(),
+  ],
 })
